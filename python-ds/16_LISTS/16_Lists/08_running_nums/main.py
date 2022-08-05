9a5d4877cc6f82323d4a3f3b5394e29416f03267
Obolensk <menshaev@yandex.ru>
2022-08-05 19:14:35 +0300

    The task 16_8 is not done. Intermediate commit

--- python-ds/16_LISTS/16_Lists/08_running_nums/main.py
+++ python-ds/16_LISTS/16_Lists/08_running_nums/main.py
@@ -1,1 +1,12 @@
 # TODO здесь писать код
+
+initial_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+shift = int(input('Сдвиг: '))
+new_list = []
+
+for a in range(shift)
+    new_list.append(initial_list[])
+
+for i in range(len(initial_list)):
+    new_list.append(initial_list[i])
+print(new_list)
