bfa4b8bdca96a6bd3c0524ec3bd35ba317e2949a
Obolensk <menshaev@yandex.ru>
2022-08-05 18:52:23 +0300

    The task 16_6 is done

--- python-ds/16_LISTS/16_Lists/06_word_analysis/main.py
+++ python-ds/16_LISTS/16_Lists/06_word_analysis/main.py
@@ -5,17 +5,16 @@ new_list = []
 
 for sym in word:
     new_list.append(sym)
-print('new_list - ', new_list)
-print('word - ', word)
-
+# print('new_list - ', new_list)
+# print('word - ', word)
+uniq = 0
 count = 0
-for i in new_list:
-    int_list = new_list.remove(i)
-    if i in int_list:
-        count += 1
-        print(i)
-
-#
-# for i in range(len(new_list)):
-#     if new_list[i] in word:
-#         count += 1
+for letter in word:
+    for i in range(len(new_list)):
+        if new_list[i] == letter:
+            count += 1
+    # print(letter, 'встречается в слове', count, 'раз')
+    if count == 1:
+        uniq += 1
+    count = 0
+print('Количество уникальных букв: ', uniq)
