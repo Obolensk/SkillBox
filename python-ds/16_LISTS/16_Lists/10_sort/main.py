d84add965591aff3bc67278c8e53748b2d015b26
Obolensk <menshaev@yandex.ru>
2022-08-08 12:49:51 +0300

    The task 16_10 is not done yet. Intermediate commit

--- python-ds/16_LISTS/16_Lists/10_sort/main.py
+++ python-ds/16_LISTS/16_Lists/10_sort/main.py
@@ -1,1 +1,9 @@
 # TODO здесь писать код
+# my_list = input('Изначальный список: ')
+my_list = [1, 2, 3, 4, 5]
+print(my_list)
+for i in range(len(my_list)):
+    if my_list[i] > my_list[i-1]:
+        my_list[i-1] = my_list[i]
+    print(my_list[i])
+print(my_list)
