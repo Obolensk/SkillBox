3d3e07bba025141c1e2684b6999826a430e8077a
Obolensk <menshaev@yandex.ru>
2022-08-09 13:29:29 +0300

    The task 16_10 is  done.

--- python-ds/16_LISTS/16_Lists/10_sort/main.py
+++ python-ds/16_LISTS/16_Lists/10_sort/main.py
@@ -1,10 +1,6 @@
 # TODO здесь писать код
 # my_list = input('Изначальный список: ')
-my_list = [1, 2, 3, 4, 5]
-print(my_list)
-for i in range(len(my_list)):
-    if my_list[i] > my_list[i-1]:
-        my_list[i-1] = my_list[i]
-    print(my_list[i])
-print(my_list)
+my_list = [1, 4, -3, 0, 10]
+print('Изначальный список: ', my_list)
+print('Отсортированный список: ', sorted(my_list))
 
