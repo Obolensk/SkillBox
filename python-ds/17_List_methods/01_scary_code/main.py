c101aa0c68224036b9911b1f88865754d31194a4
Obolensk <menshaev@yandex.ru>
2022-08-11 13:10:51 +0300

    The task 17_1 is done

--- python-ds/17_List_methods/01_scary_code/main.py
+++ python-ds/17_List_methods/01_scary_code/main.py
@@ -1,24 +1,28 @@
 a = [1, 5, 3]
 b = [1, 5, 1, 5]
 c = [1, 3, 1, 5, 3, 3]
-for i in b:
-    a.append(i)
-t = 0
+
+a.extend(b)
+# print(a)
+fives = 0
+three = 0
 for i in a:
     if i == 5:
-        t += 1
-print(t)
-d = []
+        fives += 1
+
+print('Кол-во цифр 5 при первом объединении: ', fives)
+
+for i in range(fives):
+    a.remove(5)
+# print(a)
+
+a.extend(c)
+# print(a)
+
 for i in a:
-    if i != 5:
-        d.append(i)
-for i in c:
-    d.append(i)
-t = 0
-for i in d:
     if i == 3:
-        t += 1
-print(t)
-print(d)
+        three += 1
+print('Кол-во цифр 3 при втором объединении: ', three)
+print('Итоговый список: ', a)
 
 # TODO переписать программу
