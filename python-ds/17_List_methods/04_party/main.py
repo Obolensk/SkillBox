5f49d12e03a28471c7eabdcefe14f6e52e2c4f28
Obolensk <menshaev@yandex.ru>
2022-08-12 10:09:20 +0300

    The task 17_4 is done And branch 17_04 added

--- python-ds/17_List_methods/04_party/main.py
+++ python-ds/17_List_methods/04_party/main.py
@@ -1,3 +1,20 @@
 guests = ['Петя', 'Ваня', 'Саша', 'Лиза', 'Катя']
 
 # TODO здесь писать код
+
+ans = ''
+while True:
+    print('Сейчас на вечеринке', len(guests), 'человек:', guests)
+    ans = input('Гость пришёл или ушёл? ')
+    if ans == 'Пора спать':
+        print('Вечеринка закончилась, все легли спать.')
+        break
+    name = input('Имя гостя: ')
+    if ans == 'пришёл':
+        if len(guests) > 5:
+            print('Прости,', name, ', но мест нет.')
+        else:
+            guests.append(name)
+    if ans == 'ушёл':
+        guests.remove(name)
+        print('Пока,', name, '!')
