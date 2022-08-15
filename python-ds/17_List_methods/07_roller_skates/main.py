068f140878051b86ae5517054e359294a8edad85
Obolensk <menshaev@yandex.ru>
2022-08-15 09:25:01 +0300

    The task 17_7 is done

--- python-ds/17_List_methods/07_roller_skates/main.py
+++ python-ds/17_List_methods/07_roller_skates/main.py
@@ -1,8 +1,8 @@
 # TODO здесь писать код
 
 rol_nums = int(input('Кол-во коньков: '))
-rol_sizes = ["""41, 40, 39, 42"""]
-feet_sizes = ["""42, 41, 42"""]
+rol_sizes = []
+feet_sizes = []
 total = 0
 
 for i in range(rol_nums):
@@ -16,24 +16,15 @@ for i in range(hum_nums):
     foot_size = int(input('-го человека: '))
     feet_sizes.append(foot_size)
 
-# print('rol_sizes', rol_sizes)
-# print('feet_sizes', feet_sizes)
-# print('len(rol_sizes)', len(rol_sizes))
-
-
 x = len(rol_sizes)
 for foot in feet_sizes:
-    # print('foot',foot)
     for i in range(x):
-        # print()
-        print('foot', type(foot))
-        print('rol_sizes[i]', type(rol_sizes[i]))
         if foot <= rol_sizes[i]:
             rol_sizes.remove(rol_sizes[i])
             total += 1
             x -= 2
-        # print('total', total)
 
+print()
 print('Наибольшее кол-во людей, которые могут взять ролики: ', total)
 
 
