e80b51b9c6846c3d775a0e0ed4a171f92719b74b
Obolensk <menshaev@yandex.ru>
2022-08-29 00:14:19 +0300

    The task 18_01 is done.

--- python-ds/18_List_comprehensions/01_vowels/main.py
+++ python-ds/18_List_comprehensions/01_vowels/main.py
@@ -1,1 +1,7 @@
 # TODO здесь писать код
+
+my_text = input('Введите текст: ')
+vowels = ['а', 'и', 'е', 'ё', 'о', 'у', 'ы', 'э', 'ю', 'я']
+list = [sym for sym in my_text if sym in vowels]
+print('Список гласных букв:', list)
+print('Длина списка:', len(list))
