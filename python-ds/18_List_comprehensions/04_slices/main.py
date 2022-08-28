5e951917757a7774107b7d33c5ba7a9b6192c1d2
Obolensk <menshaev@yandex.ru>
2022-08-29 00:38:10 +0300

    The task 18_04 is done.

--- python-ds/18_List_comprehensions/04_slices/main.py
+++ python-ds/18_List_comprehensions/04_slices/main.py
@@ -1,3 +1,14 @@
 alphabet = 'abcdefg'
 
 # TODO здесь писать код
+
+print('1:', alphabet[:])
+print('2:', alphabet[::-1])
+print('3:', alphabet[::2])
+print('4:', alphabet[1::2])
+print('5:', alphabet[:1])
+print('6:', alphabet[6:])
+print('7:', alphabet[3:4])
+print('8:', alphabet[4:])
+print('9:', alphabet[3:5])
+print('10:', alphabet[4:2:-1])
