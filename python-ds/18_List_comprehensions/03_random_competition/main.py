49fd9bb40fd29b1982f820fa5f179a31af9b8777
Obolensk <menshaev@yandex.ru>
2022-08-29 00:29:59 +0300

    The task 18_03 is done.

--- python-ds/18_List_comprehensions/03_random_competition/main.py
+++ python-ds/18_List_comprehensions/03_random_competition/main.py
@@ -1,1 +1,11 @@
 # TODO здесь писать код
+
+import random
+
+first_team = [random.randint(500, 1000)/100 for _ in range(20)]
+second_team = [random.randint(500, 1000)/100 for _ in range(20)]
+print('Первая команда:', first_team)
+print('Вторая команда:', second_team)
+
+winners = [(first_team[i] if first_team[i] > second_team[i] else second_team[i]) for i in range(20)]
+print('Победители тура:', winners)
