629e453fa9c3cbd43036b50e68cdd3a512ed8fef
Obolensk <menshaev@yandex.ru>
2022-09-01 14:44:45 +0300

    The task 18_05 is not done yet. Intermediate commit 01.09.22

--- python-ds/18_List_comprehensions/05_reversal/main.py
+++ python-ds/18_List_comprehensions/05_reversal/main.py
@@ -6,3 +6,11 @@ new_str = []
 
 print(rev_str)
 
+for i in range(len(rev_str)):
+    if rev_str[i] != 'h':
+        continue
+    else:
+        for j in range(i, len(rev_str)):
+            new_str.append(rev_str[j])
+
+print(new_str)
