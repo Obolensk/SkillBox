374b80b89eaddc29f47fd0f1c922e9cc17da19be
Obolensk <menshaev@yandex.ru>
2022-09-12 00:45:47 +0300

    The task 18_08 is done.

--- python-ds/18_List_comprehensions/08_entertainment/main.py
+++ python-ds/18_List_comprehensions/08_entertainment/main.py
@@ -1,1 +1,16 @@
 # TODO здесь писать код
+
+N = int(input('Количество палок: ',))
+sticks = ['I' for i in range(N)]
+
+print(''.join(sticks))
+
+throws = int(input('Количество бросков: ', ))
+
+for i in range(throws):
+    print('Бросок', i+1, '.', end=' ')
+    downed_1 = int(input('Сбиты палки с номера ', ))
+    downed_2 = int(input('по номер ', ))
+    sticks[downed_1-1:downed_2] = ['.'] * (downed_2 - downed_1 + 1)
+
+print(''.join(sticks))
