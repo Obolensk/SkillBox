0afbf2634e178c83d40321aa2e9b9431357e2425
Obolensk <menshaev@yandex.ru>
2022-09-12 00:16:49 +0300

    The task 18_06 is done.

--- python-ds/18_List_comprehensions/06_list_compression/main.py
+++ python-ds/18_List_comprehensions/06_list_compression/main.py
@@ -1,1 +1,12 @@
 # TODO здесь писать код
+
+import random
+
+n = int(input('Количество чисел в списке: '))
+list = [random.randint(0, 2) for i in range(n)]
+
+print('Список до сжатия:', list)
+
+new_list = [i for i in list if i != 0]
+
+print('Список после сжатия:', new_list)
