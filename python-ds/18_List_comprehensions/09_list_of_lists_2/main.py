cf27ec400f1c26aff2dca62f8555f349f0c8053a
Obolensk <menshaev@yandex.ru>
2022-09-12 00:58:10 +0300

    The task 18_09 is not done yet. Intermediate commit 12.09.22 0:58

--- python-ds/18_List_comprehensions/09_list_of_lists_2/main.py
+++ python-ds/18_List_comprehensions/09_list_of_lists_2/main.py
@@ -2,3 +2,9 @@ nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
              [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
 
 # TODO здесь писать код
+
+
+new_list = [i for i in nice_list]
+
+print('nice_list = ', nice_list)
+print('new_list = ', new_list)
