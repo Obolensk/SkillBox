d5a0b92b9b27a011195d96e96943801b0bf56710
Obolensk <menshaev@yandex.ru>
2022-09-12 12:17:52 +0300

    The task 18_09 is done.

--- python-ds/18_List_comprehensions/09_list_of_lists_2/main.py
+++ python-ds/18_List_comprehensions/09_list_of_lists_2/main.py
@@ -3,8 +3,14 @@ nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
 
 # TODO здесь писать код
 
-
-new_list = [i for i in nice_list]
+new_list = [list_1 for list_2 in nice_list for list_3 in list_2 for list_1 in list_3]
 
 print('nice_list = ', nice_list)
 print('new_list = ', new_list)
+
+# new = []
+# for i in nice_list:
+#     for n in i:
+#         for a in n:
+#             new.append(a)
+
