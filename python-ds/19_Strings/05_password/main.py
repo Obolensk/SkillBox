efb32b07596089a3a967ca3cd6848668430dc343
Obolensk <menshaev@yandex.ru>
2022-09-14 00:39:23 +0300

    The task 19_05 is done.

--- python-ds/19_Strings/05_password/main.py
+++ python-ds/19_Strings/05_password/main.py
@@ -1,1 +1,19 @@
 # TODO здесь писать код
+
+while True:
+    password = input('Придумайте пароль: ')
+    letter_count = 0
+    up_case = 0
+    nums = 0
+
+    for letter in password:
+        if letter.isupper():
+            up_case += 1
+        if letter.isdigit():
+            nums += 1
+        letter_count += 1
+    if letter_count < 8 or up_case < 1 or nums < 3:
+        print('Пароль ненадёжный. Попробуйте ещё раз.')
+    else:
+        print('Это надёжный пароль!')
+        break
