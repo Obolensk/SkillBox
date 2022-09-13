d9580c6c530496f25d6d24b8b65b62e33c9dea1d
Obolensk <menshaev@yandex.ru>
2022-09-14 01:01:22 +0300

    The task 19_05 is note done yet. Intermediate commit 14.09.22 1:01

--- python-ds/19_Strings/06_compression/main.py
+++ python-ds/19_Strings/06_compression/main.py
@@ -1,1 +1,22 @@
 # TODO здесь писать код
+
+my_str = input('Введите строку: ')
+decoded = []
+new_str = []
+
+for letter in my_str:
+    new_str.append(letter)
+
+print(new_str)
+
+letter_count = 1
+for i in range(len(new_str)-1):
+    if new_str[i] == new_str[i+1]:
+        letter_count += 1
+        decoded.append(new_str[i] + str(letter_count))
+        letter_count = 1
+    else:
+        decoded.append(new_str[i] + '1')
+
+print(decoded)
+
