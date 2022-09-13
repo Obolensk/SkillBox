d0f25dec8388d0efdb4113131898b5c815596972
Obolensk <menshaev@yandex.ru>
2022-09-13 23:40:04 +0300

    The task 19_02 is done.

--- python-ds/19_Strings/02_longest_word/main.py
+++ python-ds/19_Strings/02_longest_word/main.py
@@ -1,1 +1,14 @@
 # TODO здесь писать код
+
+my_text = input('Введите строку: ').split()
+
+letter_count = 0
+letter_count_list = []
+for word in my_text:
+    for letter in word:
+        letter_count += 1
+    letter_count_list.append(letter_count)
+    letter_count = 0
+
+print('Самое длинное слово: {0}'.format(my_text[letter_count_list.index(max(letter_count_list))]))
+print('Длина этого слова: {0}'.format(max(letter_count_list)))
