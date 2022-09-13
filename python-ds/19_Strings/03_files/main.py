025f0fa1385d45a7c2ca457bb88849f82a4a8fbf
Obolensk <menshaev@yandex.ru>
2022-09-14 00:14:05 +0300

    The task 19_03 is done.

--- python-ds/19_Strings/03_files/main.py
+++ python-ds/19_Strings/03_files/main.py
@@ -1,1 +1,19 @@
 # TODO здесь писать код
+
+name = input('Название файла: ')
+
+sym_list = ['@', '№', '$', '%', '^', '&', '*', '()']
+ext_list = ['.txt', '.docx']
+
+for sym in sym_list:
+    if name.startswith(sym):
+        print('Ошибка: название начинается на один из специальных символов.')
+        break
+    else:
+        if name.endswith('.txt') or name.endswith('.docx'):
+            print('Файл назван верно.')
+            break
+        else:
+            print('Ошибка: неверное расширение файла. Ожидалось .txt или .docx.')
+            break
+
