c330e404f8da5476323a64db9edd2e80eed0fc47
Obolensk <menshaev@yandex.ru>
2022-09-15 00:51:06 +0300

    The task 19_08 is done.

--- python-ds/19_Strings/08_ticker/main.py
+++ python-ds/19_Strings/08_ticker/main.py
@@ -1,1 +1,29 @@
 # TODO здесь писать код
+
+fst_text = input('Первая строка: ')
+fst_str = []
+for letter in fst_text:
+    fst_str.append(letter)
+# print(fst_str)
+
+sec_text = input('Вторая строка: ')
+sec_str = []
+for letter in sec_text:
+    sec_str.append(letter)
+# print(sec_str)
+
+def shift(str):
+    new_str = [(str[i + 1] if i != (len(str) - 1) else str[0]) for i in range(len(str))]
+    return new_str
+
+shift_count = 0
+my_str = fst_str[:]
+for i in range(len(my_str)):
+    my_str = shift(my_str)
+    shift_count += 1
+    # print(my_str)
+    if my_str == sec_str:
+        print('Первая строка получается из второй со сдвигом', shift_count)
+        break
+if shift_count == len(my_str):
+    print('Первую строку нельзя получить из второй с помощью циклического сдвига')
