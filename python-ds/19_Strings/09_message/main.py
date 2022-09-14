21047bc266a9b9948cd98773b508911877c1c717
Obolensk <menshaev@yandex.ru>
2022-09-15 01:22:40 +0300

    The task 19_09 is done.

--- python-ds/19_Strings/09_message/main.py
+++ python-ds/19_Strings/09_message/main.py
@@ -1,1 +1,18 @@
 # TODO здесь писать код
+
+# msg = input('Сообщение: ').split()
+msg = 'Это задание очень! простое.'.split()
+# syms = ['.', ',', '!', '?', ':', ';']
+
+# print(msg)
+# print(msg[1])
+
+list = []
+for word in msg:
+    for letter in word:
+        list.append(letter)
+    print(''.join(list[::-1]), end=' ')
+    list = []
+
+#
+
