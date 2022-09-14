60bd938a0120d3b44f20072a12928245133d1c6f
Obolensk <menshaev@yandex.ru>
2022-09-14 14:10:32 +0300

    The task 19_07 is note done yet. Intermediate commit 14.09.22 14:10

--- python-ds/19_Strings/07_ip_adress_2/main.py
+++ python-ds/19_Strings/07_ip_adress_2/main.py
@@ -1,1 +1,26 @@
 # TODO здесь писать код
+
+while True:
+    ip_adress = input('Введите IP: ').split('.')
+    print(ip_adress)
+    num_count = 0
+    for num in ip_adress:
+        num_count += 1
+        if not num.isdigit():
+            print('{0} — это не целое число'.format(num))
+            break
+        elif int(num) < 0:
+            print('{0} меньше 0'.format(num))
+            break
+        elif int(num) > 255:
+            print('{0} првышает 255'.format(num))
+            break
+        elif not num.isdigit():
+            print('{0} — это не целое число'.format(num))
+            break
+        # elif num_count != 4:
+        #     print('Адрес — это четыре числа, разделённые точками.')
+        else:
+            print('IP-адрес корректен.')
+
+    break
