a3d719e21c84675235a0d568a51d4f5eea177fd9
Obolensk <menshaev@yandex.ru>
2022-10-01 16:47:43 +0300

    Added new lesson folder from SkillBox 20_Dictionaries_and_sets

---
+++ python-ds/20_Dictionaries_and_sets/01_songs_2/main.py
@@ -0,0 +1,13 @@
+violator_songs = {
+    'World in My Eyes': 4.86,
+    'Sweetest Perfection': 4.43,
+    'Personal Jesus': 4.56,
+    'Halo': 4.9,
+    'Waiting for the Night': 6.07,
+    'Enjoy the Silence': 4.20,
+    'Policy of Truth': 4.76,
+    'Blue Dress': 4.29,
+    'Clean': 5.83
+}
+
+# TODO здесь писать код
---
+++ python-ds/20_Dictionaries_and_sets/03_cryptocurrency/main.py
@@ -0,0 +1,48 @@
+data = {
+    "address": "0x544444444444",
+    "ETH": {
+        "balance": 444,
+        "totalIn": 444,
+        "totalOut": 4
+    },
+    "count_txs": 2,
+    "tokens": [
+        {
+            "fst_token_info": {
+                "address": "0x44444",
+                "name": "fdf",
+                "decimals": 0,
+                "symbol": "dsfdsf",
+                "total_supply": "3228562189",
+                "owner": "0x44444",
+                "last_updated": 1519022607901,
+                "issuances_count": 0,
+                "holders_count": 137528,
+                "price": False
+            },
+            "balance": 5000,
+            "totalIn": 0,
+            "totalOut": 0
+        },
+        {
+            "sec_token_info": {
+                "address": "0x44444",
+                "name": "ggg",
+                "decimals": "2",
+                "symbol": "fff",
+                "total_supply": "250000000000",
+                "owner": "0x44444",
+                "last_updated": 1520452201,
+                "issuances_count": 0,
+                "holders_count": 20707,
+                "price": False
+            },
+            "balance": 500,
+            "totalIn": 0,
+            "total_out": 0
+        }
+    ]
+}
+
+
+# TODO здесь писать код
