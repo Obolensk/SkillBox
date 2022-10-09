04c766685cdc9d926032276f7f98311ddb97c55a
Obolensk <menshaev@yandex.ru>
2022-10-09 17:00:09 +0300

    The task 20_05 is done.

--- python-ds/20_Dictionaries_and_sets/05_frequency_hist_2/main.py
+++ python-ds/20_Dictionaries_and_sets/05_frequency_hist_2/main.py
@@ -1,1 +1,37 @@
 # TODO здесь писать код
+
+my_dict = {
+  ' ': 2,
+  '-': 1,
+  'З': 1,
+  'а': 2,
+  'д': 1,
+  'е': 1,
+  'и': 1,
+  'н': 2,
+  'о': 3,
+  'п': 1,
+  'с': 2,
+  'т': 2,
+  'ч': 1,
+  'ь': 1
+}
+
+vals = []
+for val in my_dict.values():
+  if val not in vals:
+    vals.append(val)
+
+res = dict()
+my_list = []
+
+for k in vals:
+  res[k] = my_list
+  for v in my_dict.keys():
+    if my_dict[v] == k:
+      my_list.append(v)
+  my_list = []
+
+print(res)
+
+
