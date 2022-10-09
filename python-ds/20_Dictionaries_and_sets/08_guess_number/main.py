a919dc5dca4bf40a9b4069d0f87e4ffe6031292a
Obolensk <menshaev@yandex.ru>
2022-10-09 17:03:17 +0300

    The task 20_08 is done.

--- python-ds/20_Dictionaries_and_sets/08_guess_number/main.py
+++ python-ds/20_Dictionaries_and_sets/08_guess_number/main.py
@@ -1,1 +1,31 @@
 # TODO здесь писать код
+
+import random
+
+n = int(input('Введите максимальное число: '))
+# print(nums)
+
+ans = random.randint(1, n + 1)
+print(ans)
+
+answers = []
+for i in range(1, n + 1):
+  answers.append(i)
+print(answers)
+
+
+while True:
+  nums = input('Нужное число есть среди вот этих чисел: ').split()
+  if 'f' in nums:
+    print('Артём мог загадать следующие числа:', answers)
+    break
+  elif str(ans) in nums:
+    print('Ответ Артёма: Да')
+    break
+  else:
+    for i in nums:
+      if int(i) in answers:
+        answers.remove(int(i))
+      else:
+        continue
+    print('Ответ Артёма: Нет')
