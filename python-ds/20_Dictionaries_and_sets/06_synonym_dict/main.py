218b2b2a2da43a9ea8ea495a000e98af1fdaeaaa
Obolensk <menshaev@yandex.ru>
2022-10-09 17:00:59 +0300

    The task 20_06 is done.

--- python-ds/20_Dictionaries_and_sets/06_synonym_dict/main.py
+++ python-ds/20_Dictionaries_and_sets/06_synonym_dict/main.py
@@ -1,1 +1,24 @@
 # TODO здесь писать код
+
+
+word_nums = int(input('Введите количество пар слов: '))
+
+word_dict = dict()
+for i in range(word_nums):
+  print(i + 1, 'пара: ', end = '')
+  pair = input('').split(' - ')
+  word_dict[pair[0].lower()] = pair[1].lower()
+
+my_keys = list(word_dict.keys())
+my_values = list(word_dict.values())
+
+while True:
+  new_word = input('Введите слово: ').lower()
+  if new_word in word_dict.keys():
+    print('Синоним:', word_dict[new_word])
+    break
+  elif new_word in word_dict.values():
+    print('Синоним:', my_keys[my_values.index(new_word)])
+    break
+  else:
+    print('Такого слова в словаре нет.')
