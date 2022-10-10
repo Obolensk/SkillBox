b2fef1c2ce89be7e17a98f58b1647eae0b28dd28
Obolensk <menshaev@yandex.ru>
2022-10-10 21:14:20 +0300

    The task 20_09 is done.

--- python-ds/20_Dictionaries_and_sets/09_pedigree/main.py
+++ python-ds/20_Dictionaries_and_sets/09_pedigree/main.py
@@ -1,1 +1,31 @@
 # TODO здесь писать код
+
+# Задача 9
+
+# Алгоритм такой:
+# 1. При вводе списка, добавляем оба элемента в словарь как ключи, а значения по умолчанию у первого элемента - ноль, а у нулевого - значение первого элемента + 1.
+
+pers_nums = int(input('Введите количество человек: '))
+pers_dict = dict()
+
+for i in range(pers_nums - 1):
+  print(i + 1, end=' ')
+  pair = input('пара: ').split()
+  # print(pair)
+  if pair[1] in pers_dict:
+    pers_dict[pair[0]] = pers_dict[pair[1]] + 1
+  else:
+    pers_dict[pair[1]] = 0
+    pers_dict[pair[0]] = pers_dict[pair[1]] + 1
+  # print()
+  # print(pers_dict)
+# print()
+# print(sorted(pers_dict.items()))
+
+print()
+print('“Высота” каждого члена семьи:')
+for pers in sorted(pers_dict.items()):
+  for i in pers:
+    print(i, end=' ')
+  print()
+
