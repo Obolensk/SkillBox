7aa8c8d9ee6dc7ee95fb25e0521f79692880e4af
Obolensk <menshaev@yandex.ru>
2022-10-10 21:15:34 +0300

    The task 20_07 is done.

--- python-ds/20_Dictionaries_and_sets/07_pizza/main.py
+++ python-ds/20_Dictionaries_and_sets/07_pizza/main.py
@@ -1,1 +1,25 @@
 # TODO здесь писать код
+
+
+order_nums = int(input('Введите кол-во заказов: '))
+
+order_dict = {}
+
+for i in range(order_nums):
+  print(i + 1, 'заказ: ', end = '')
+  pair = input('').split()
+  if pair[0] not in order_dict:
+    order_dict[pair[0]] = {pair[1]: int(pair[2])}
+  else:
+    if pair[1] not in order_dict[pair[0]]:
+      order_dict[pair[0]][pair[1]] = int(pair[2])
+    else:
+      order_dict[pair[0]][pair[1]] += int(pair[2])
+
+print()
+
+for name in sorted(order_dict):
+  print(name, ':')
+  for pizza in order_dict[name]:
+    print('\t', pizza, ':', order_dict[name][pizza])
+
