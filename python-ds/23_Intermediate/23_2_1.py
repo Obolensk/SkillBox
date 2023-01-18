64c6e99108a9a1a3bde1a915c3399c1b279e7e85
Obolensk <menshaev@yandex.ru>
2023-01-18 16:14:17 +0300

    The tasks 23_2_1 is not done yet.

--- python-ds/23_Intermediate/23_2_1.py
+++ python-ds/23_Intermediate/23_2_1.py
@@ -1,29 +1,26 @@
 import os
 
-# print(os.path.join('..', os.getcwd()))
-#
-# print(os.path.abspath('/'))
-# print(os.listdir('/'))
 
-print(os.listdir('C:\Program Files'))
-print(os.path.isdir('C:\Program Files'))
-print(os.path.isfile('C:\Program Files'))
+print(os.listdir('C:/Users/e.menshaev/Desktop'))
+# print(os.path.isdir('C:\Program Files'))
+# print(os.path.isfile('C:\Program Files'))
+mypath = os.path.abspath('C:/Users/e.menshaev/Desktop')
 print()
 
-for fold in os.listdir('C:/Users/e.menshaev/Desktop/Skillbox'):
-    # print(fold)
-    # print(os.path.join('C:', os.path.sep, fold))
-    # print('os.path.isfile(fold) = ', os.path.isfile(os.path.join('C:', os.path.sep, fold)))
-    # print('os.path.isdir(fold) = ', os.path.isdir(os.path.join('C:', os.path.sep, fold)))
-    # print('os.path.exists(fold) = ', os.path.exists(os.path.join('C:', os.path.sep, fold)))
-    if os.path.isfile(os.path.join('C:', os.path.sep, fold)):
-        print(os.path.join('C:', os.path.sep, fold))
+for fold in os.listdir(mypath):
+    print(fold)
+    print(os.path.join(mypath, os.path.sep, fold))
+    print('os.path.isfile(fold) = ', os.path.isfile(os.path.join(mypath, os.path.sep, fold)))
+    print('os.path.isdir(fold) = ', os.path.isdir(os.path.join(mypath, os.path.sep, fold)))
+    print('os.path.exists(fold) = ', os.path.exists(os.path.join(mypath, os.path.sep, fold)))
+    if os.path.isfile(os.path.join('C:/Users/e.menshaev/Desktop', os.path.sep, fold)):
+        print(os.path.join('C:/Users/e.menshaev/Desktop', os.path.sep, fold), end=' - ')
         print('Это файл')
     else:
         # print('os.path.isfile(os.path.join(fold)', os.path.isfile(os.path.join('C:', os.path.sep, fold)))
         # print('os.path.isdir(os.path.join(fold)', os.path.isdir(os.path.join('C:', os.path.sep, fold)))
-        print(os.path.join('C:', os.path.sep, fold))
+        print(os.path.join('C:/Users/e.menshaev/Desktop', os.path.sep, fold), end=' - ')
         print('Это папка')
-        print('Вот её содержимое:')
-        print(os.listdir(os.path.join('C:', os.path.sep, fold)))
-        print()
+        # print('Вот её содержимое:')
+        # print(os.listdir(os.path.join('C:', os.path.sep, fold)))
+        # print()
