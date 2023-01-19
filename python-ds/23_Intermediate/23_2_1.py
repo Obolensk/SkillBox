6e214cbb527750d3dbfb8bc442494c06feedc1f3
Obolensk <menshaev@yandex.ru>
2023-01-19 15:53:00 +0300

    new task 23_2_1 is done

--- python-ds/23_Intermediate/23_2_1.py
+++ python-ds/23_Intermediate/23_2_1.py
@@ -1,26 +1,17 @@
 import os
 
 
-print(os.listdir('C:/Users/e.menshaev/Desktop'))
-# print(os.path.isdir('C:\Program Files'))
-# print(os.path.isfile('C:\Program Files'))
-mypath = os.path.abspath('C:/Users/e.menshaev/Desktop')
-print()
+print(os.listdir('C:/Users/e.menshaev/Desktop/Skillbox/python-ds/23_Intermediate/'))
 
-for fold in os.listdir(mypath):
-    print(fold)
-    print(os.path.join(mypath, os.path.sep, fold))
-    print('os.path.isfile(fold) = ', os.path.isfile(os.path.join(mypath, os.path.sep, fold)))
-    print('os.path.isdir(fold) = ', os.path.isdir(os.path.join(mypath, os.path.sep, fold)))
-    print('os.path.exists(fold) = ', os.path.exists(os.path.join(mypath, os.path.sep, fold)))
-    if os.path.isfile(os.path.join('C:/Users/e.menshaev/Desktop', os.path.sep, fold)):
-        print(os.path.join('C:/Users/e.menshaev/Desktop', os.path.sep, fold), end=' - ')
-        print('Это файл')
+for dirs in os.listdir('C:/Users/e.menshaev/Desktop/Skillbox/python-ds/23_Intermediate/'):
+    print()
+    print(dirs)
+    print(os.path.abspath(dirs))
+    # print(os.path.join('C:/Users/e.menshaev/Desktop/Skillbox/python-ds/23_Intermediate/', os.path.sep, dirs))
+    if os.path.exists(os.path.abspath(dirs)):
+        if os.path.isfile(os.path.abspath(dirs)):
+            print('   Это файл')
+        else:
+            print('   Скорее всего это уже папка')
     else:
-        # print('os.path.isfile(os.path.join(fold)', os.path.isfile(os.path.join('C:', os.path.sep, fold)))
-        # print('os.path.isdir(os.path.join(fold)', os.path.isdir(os.path.join('C:', os.path.sep, fold)))
-        print(os.path.join('C:/Users/e.menshaev/Desktop', os.path.sep, fold), end=' - ')
-        print('Это папка')
-        # print('Вот её содержимое:')
-        # print(os.listdir(os.path.join('C:', os.path.sep, fold)))
-        # print()
+        print('Указанного пути не существует')
