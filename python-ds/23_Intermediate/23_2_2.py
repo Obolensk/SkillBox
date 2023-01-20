6782ace183611ea026270b7251249f6ef55ebcde
Obolensk <menshaev@yandex.ru>
2023-01-20 16:49:50 +0300

    The task 23_3_2 is done

--- python-ds/23_Intermediate/23_2_2.py
+++ python-ds/23_Intermediate/23_2_2.py
@@ -1,7 +1,11 @@
 import os
+import random
 
 my_dir = 'C:/Users/e.menshaev/Desktop/Skillbox/python-ds/15_IDE/'
 file_name = 'main.py'
+count = random.randint(2, 4)
+print(count)
+i = 0
 
 for dirs in os.listdir(my_dir):
     print()
@@ -11,5 +15,12 @@ for dirs in os.listdir(my_dir):
         if file == file_name:
             print('   ', os.path.join(my_dir, dirs, file), end=' - ')
             print('Размер файла -', os.path.getsize(os.path.join(my_dir, dirs, file)), 'байт!')
-
+            my_read = open(os.path.join(my_dir, dirs, file), 'r', encoding= 'UTF-8')
+            i += 1
+            if count == i:
+                my = my_read.read()
+                print(my)
+                print()
+        else:
+            continue
 
