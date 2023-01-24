5fba21cfe9c45f7df37d2c39e68d20fe4bef50c0
Obolensk <menshaev@yandex.ru>
2023-01-24 18:41:48 +0300

    The task 23_02 is done.

--- python-ds/23_Files/02_zen_of_python/main.py
+++ python-ds/23_Files/02_zen_of_python/main.py
@@ -1,1 +1,13 @@
 # TODO здесь писать код
+
+new_file = open('C:/Users/e.menshaev/Desktop/Skillbox/python-ds/23_Files/02_zen_of_python/zen.txt', 'r',
+                encoding='UTF-8')
+
+zen = new_file.read()
+zen_list = []
+
+for i in zen.split('\n'):
+    zen_list.append(i)
+
+for i in range(len(zen_list)-1, -1, -1):
+    print(zen_list[i])
