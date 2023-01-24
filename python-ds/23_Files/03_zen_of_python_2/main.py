0b2f9d2d263a22313acdd85afe4be32f9252ee7b
Obolensk <menshaev@yandex.ru>
2023-01-24 19:14:17 +0300

    The task 23_03 is note done yet. I have to find out how to find the letters in all this symbols

--- python-ds/23_Files/03_zen_of_python_2/main.py
+++ python-ds/23_Files/03_zen_of_python_2/main.py
@@ -1,1 +1,49 @@
 # TODO здесь писать код
+
+new_file = open('C:/Users/e.menshaev/Desktop/Skillbox/python-ds/23_Files/02_zen_of_python/zen.txt', 'r',
+                encoding='UTF-8')
+
+let = 0
+words = 0
+strs = 0
+r_let = 10
+l_count = 0
+
+
+zen = new_file.read()
+zen_list = []
+
+for l in zen:
+    let += 1
+print('Количество букв в файле:', let)
+
+for w in zen.split():
+    words += 1
+print('Количество слов в файле:', words)
+
+for s in zen.split('\n'):
+    strs += 1
+print('Количество строк в файле:', strs)
+
+for i in zen:
+    zen_list.append(i)
+
+print(zen_list)
+print((len(zen_list)))
+
+
+for a in range(len(zen_list)):
+    for b in range(len(zen_list)):
+        if zen_list[a].lower() == zen_list[b].lower():
+            l_count += 1
+    print('Количество букв ', zen_list[a].lower(), 'равняется', l_count)
+    if l_count < r_let:
+        r_let = l_count
+        my_let = zen_list[b].lower()
+    l_count = 0
+
+print()
+print('r_let = ', r_let)
+print()
+print('my_let = ', my_let)
+
