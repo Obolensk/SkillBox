88e87fc29fbab8511fe1e90a1961ee1d9b509426
Obolensk <menshaev@yandex.ru>
2023-01-26 18:31:23 +0300

    The task 23_04 is done.

--- python-ds/23_Files/04_files_and_folders/main.py
+++ python-ds/23_Files/04_files_and_folders/main.py
@@ -1,1 +1,27 @@
 # TODO здесь писать код
+
+import os
+files = 0
+folders = 0
+size = 0
+
+
+# print('1 = ', os.listdir('C:/Users/e.menshaev/Desktop/Skillbox/python-ds/23_Intermediate/'))
+
+for dirs in os.listdir('C:/Users/e.menshaev/Desktop/Skillbox/python-ds/23_Intermediate/'):
+    # print()
+    # print('2 = ', dirs)
+    # print('3 = ', os.path.abspath(dirs))
+    # print('os.path.isfile(os.path.abspath(dirs)) =', os.path.isfile(os.path.join('C:/Users/e.menshaev/Desktop/Skillbox/python-ds/23_Intermediate/', dirs)))
+    if os.path.isfile(os.path.join('C:/Users/e.menshaev/Desktop/Skillbox/python-ds/23_Intermediate/', dirs)):
+        # print('   Это файл')
+        files += 1
+        size += os.path.getsize(os.path.join('C:/Users/e.menshaev/Desktop/Skillbox/python-ds/23_Intermediate/', dirs))
+    else:
+        # print('   Скорее всего это уже папка')
+        folders += 1
+        size += os.path.getsize(os.path.join('C:/Users/e.menshaev/Desktop/Skillbox/python-ds/23_Intermediate/', dirs))
+
+print('Количество файлов:', files)
+print('Количество подкаталогов:', folders)
+print('Размер каталога (в Кб):', size / 1024, 'Кб')
