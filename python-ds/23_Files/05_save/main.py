813bbc45df64ab26b15cd53cc9a6553e261a6140
Obolensk <menshaev@yandex.ru>
2023-01-27 18:05:12 +0300

    The task 23_05 is not done yet.

--- python-ds/23_Files/05_save/main.py
+++ python-ds/23_Files/05_save/main.py
@@ -1,1 +1,19 @@
 # TODO здесь писать код
+
+import os
+
+text = input('Введите строку: ')
+path = input('Куда хотите сохранить документ? Введите последовательность папок (через пробел):')
+my_path = 'C:/Users/e.menshaev/Desktop/Skillbox/python-ds/' + path.split()[0] + '/' + path.split()[1] + '/'
+# dirs_list = dirs.split()
+# title = input('Введите имя файла: ')
+
+
+# new_file.write(text)
+
+text = text + '.txt'
+print(text)
+print(my_path)
+print(my_path + text)
+
+# new_file = open(text, r, encoding='UTF-8')
