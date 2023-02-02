144df241ea35cd331c41009b6fb62c1e63c61f40
Obolensk <menshaev@yandex.ru>
2023-02-02 13:56:33 +0300

    The task 23_07 is not done yet. new file added

--- python-ds/23_Files/07_tournament/main.py
+++ python-ds/23_Files/07_tournament/main.py
@@ -1,6 +1,5 @@
 # TODO здесь писать код
-
-import operator
+import os
 
 file = open('C:/Users/e.menshaev/Desktop/Skillbox/python-ds/23_Files/07_tournament/first_tour.txt', 'r')
 old_file = file.read()
@@ -12,26 +11,28 @@ new_list = old_file.split('\n')
 print(new_list)
 
 for i in range(1, len(new_list)):
-    # print(new_list[i])
     print()
     for a in new_list[i].split():
-        # print(a)
         part_list.append(a)
     print(part_list)
     if part_list[2] > new_list[0]:
         winners_list.append(part_list)
     part_list = []
 
-print(winners_list)
-print()
-print(winners_list.sort(key=operator.itemgetter(1, 2)))
-
-news = sorted(operator.itemgetter(0, 1)(winners_list))
-print(news)
-
-print()
-print()
-print()
 print()
 print(winners_list)
 print(sorted(winners_list))
+print()
+text = []
+text.append(len(winners_list))
+# print(len(winners_list))
+place = 1
+for winners in sorted(winners_list):
+    print('{}) {}. {} {}'.format(place, winners[1][0], winners[0], winners[2]))
+    place += 1
+
+print()
+print()
+
+res = open('C:/Users/e.menshaev/Desktop/Skillbox/python-ds/23_Files/07_tournament/second_tour.txt', 'a')
+result = res.write(text)
