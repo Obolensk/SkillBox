7dde0d0deb3933348b9e4143d168591303d810a7
Obolensk <menshaev@yandex.ru>
2023-02-18 14:39:59 +0300

    The intermediate task 24_4_1 is done.

--- python-ds/24_Intermediate/24_4_1.py
+++ python-ds/24_Intermediate/24_4_1.py
@@ -1,4 +1,13 @@
 
 with open('people.txt', 'r', encoding='utf-8') as file:
     for name in file.read().split():
-        for lit in name
+        print(name)
+        count = 0
+        for lit in name:
+            count += 1
+        print('Количество букв в слове равно - {}'.format(count))
+        if count <= 3:
+            print('ОШИБКА. В имени меньше трех букв!!!')
+            break
+        else:
+            continue
