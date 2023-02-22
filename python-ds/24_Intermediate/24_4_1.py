429a4eb8ef56cd5eb70b58e1a6138618d2d0e8a6
Obolensk <menshaev@yandex.ru>
2023-02-22 15:52:27 +0300

    The intermediate task 24_4_1 is done.

--- python-ds/24_Intermediate/24_4_1.py
+++ python-ds/24_Intermediate/24_4_1.py
@@ -1,4 +1,11 @@
 
 with open('people.txt', 'r', encoding='utf-8') as file:
     for name in file.read().split():
-        for lit in name
+        # print()
+        sym_count = 0
+        for lit in name:
+            # print(lit)
+            sym_count += 1
+        if sym_count < 3:
+            print('Error!!! В имени {} меньше трех букв. Программа завершена'.format(name))
+            break
