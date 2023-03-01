08f0d7b66e70ee00598c81c57a6665fdab198fc9
Obolensk <menshaev@yandex.ru>
2023-03-01 18:08:30 +0300

    The task 24_03 is done.

--- python-ds/24_Exceptions/03_lucky_number/main.py
+++ python-ds/24_Exceptions/03_lucky_number/main.py
@@ -1,1 +1,19 @@
 # TODO здесь писать код
+
+import random
+
+summ = 0
+
+while summ < 777:
+    num = int(input('Введите число: '))
+    try:
+        summ += num
+        try_ex = num/random.randint(0, 12)
+    except:
+        print('Вас постигла неудача!')
+        break
+    with open('out_file.txt', 'a', encoding='utf-8') as file:
+        file.write(str(num))
+        file.write('\n')
+if summ >= 777:
+    print('Вы успешно выполнили условие для выхода из порочного цикла!')
