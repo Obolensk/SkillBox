eb01f7f937b3d03cfdbac88c231d1411e4e09b0c
Obolensk <menshaev@yandex.ru>
2023-03-13 18:41:37 +0300

    The task 24_06 is done.

--- python-ds/24_Exceptions/06_chat/main.py
+++ python-ds/24_Exceptions/06_chat/main.py
@@ -1,1 +1,22 @@
 # TODO здесь писать код
+
+user = input('Введите имя пользователя: ')
+while True:
+    act = input('Введите действие (1 - Посмотреть текущий текст чата, 2 - Отправить сообщение): ')
+    if act == '1':
+        with open('C:/Users/e.menshaev/Desktop/Skillbox/python-ds/24_Exceptions/06_chat/chat.txt', 'r', encoding='utf-8') \
+                as file:
+            print(file.read())
+    elif act == '2':
+        msg = input('Введите сообщение: ')
+        with open('C:/Users/e.menshaev/Desktop/Skillbox/python-ds/24_Exceptions/06_chat/chat.txt', 'a', encoding='utf-8') \
+                as file:
+            file.write('ПОЛЬЗОВАТЕЛЬ: {}'.format(user))
+            file.write('\n')
+            file.write('СООБЩЕНИЕ: {}'.format(msg))
+            file.write('\n')
+
+
+
+
+
