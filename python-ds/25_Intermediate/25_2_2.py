51b3452a07e7400b1a8b4db78f1a3e3a1510c097
Obolensk <menshaev@yandex.ru>
2023-03-19 00:30:08 +0300

    The intermediate task 25_2_2 is done, file is added!

---
+++ python-ds/25_Intermediate/25_2_2.py
@@ -0,0 +1,22 @@
+class Monitor():
+    name = 'Samsung'
+    matrix = 'VA'
+    res = 'WQHD'
+    freq = 60
+
+class Headphones():
+    name = 'Sony'
+    sensitive = 108
+    micro = True
+
+Mon_1 = Monitor()
+Mon_2 = Monitor()
+Mon_2.freq = 144
+Mon_3 = Monitor()
+Mon_3.freq = 70
+Mon_4 = Monitor()
+
+Head_1 = Headphones()
+Head_1.micro = False
+Head_2 = Headphones()
+Head_3 = Headphones()
