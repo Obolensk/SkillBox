27e95bc4788506e3965e5cf5d8f457c13ac54ecf
Obolensk <menshaev@yandex.ru>
2023-03-20 21:05:32 +0300

    The task 25_01 is done.

--- python-ds/25_OOP/01_fight/main.py
+++ python-ds/25_OOP/01_fight/main.py
@@ -1,1 +1,23 @@
 # TODO здесь писать код
+
+import random
+
+w1 = 100
+w2 = 100
+
+while w1 > 0 and w2 > 0:
+    if random.randint(1, 2) == 1:
+        print('Воин 1 бьет Воина 2')
+        w2 -= 10
+        print('У Воина 1 - {} очков'.format(w1))
+        print('У Воина 2 - {} очков\n'.format(w2))
+    else:
+        print('Воин 2 бьет Воина 1')
+        w1 -= 10
+        print('У Воина 1 - {} очков'.format(w1))
+        print('У Воина 2 - {} очков\n'.format(w2))
+
+if w1 > w2:
+    print('Победу одержал Воин 1!!!')
+else:
+    print('Победу одержал Воин 2!!!')
