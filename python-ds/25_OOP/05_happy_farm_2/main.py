a7acd1a565691f0936656462e2cf2bd640466ab8
Obolensk <menshaev@yandex.ru>
2023-03-23 10:52:08 +0300

    The task 25_05 is not done yet.

--- python-ds/25_OOP/05_happy_farm_2/main.py
+++ python-ds/25_OOP/05_happy_farm_2/main.py
@@ -1,1 +1,43 @@
 # TODO здесь писать код
+class Potato:
+    states = {
+        0: 'Отсутствует',
+        1: 'Росток',
+        2: 'Зеленая',
+        3: 'Зрелая'
+    }
+    def __init__(self, index):
+        self.index = index
+        self.state = 0
+    def grow(self):
+        if self.state < 3:
+            self.state += 1
+        self.print_state()
+    def is_ripe(self):
+        if self.state == 3:
+            return True
+        else:
+            return False
+    def print_state(self):
+        print('Картошка {} сейчас {}'.format(
+            self.index, Potato.states[self.state]
+        ))
+
+class PotatoGarden:
+    def __init__(self, count):
+        self.potatoes = [Potato(index) for index in range(1, count + 1)]
+    def grow_all(self):
+        print('Картошка прорастает!')
+        for i_potato in self.potatoes:
+            i_potato.grow()
+    def are_all_ripe(self):
+        if not all([i_potato.is_ripe() for i_potato in self.potatoes]):
+            print('Картошка еще не созрела!\n')
+        else:
+            print('Вся картошка созрела! Можно собирать!')
+
+my_garden = PotatoGarden(5)
+my_garden.are_all_ripe()
+for _ in range(3):
+    my_garden.grow_all()
+    my_garden.are_all_ripe()
