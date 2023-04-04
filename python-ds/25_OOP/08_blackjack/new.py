5549525c7307f02839a129394a0afa0586b43b5c
Obolensk <menshaev@yandex.ru>
2023-04-04 16:31:28 +0300

    The task 25_08 is not done yet.  I finally confused  Next time I'll have to delete all my code and run to start again

--- python-ds/25_OOP/08_blackjack/new.py
+++ python-ds/25_OOP/08_blackjack/new.py
@@ -21,38 +21,51 @@ class Deck:
                 cards = []
                 fault += 1
                 continue
-        # print('len(deck) = ', len(deck))
-        # print()
         return deck
 class Player:
     def __init__(self, name, score=0, hand_score=0):
         self.name = name
         self.score = score
         self.hand_score = hand_score
+
+class Hand:
+    def __init__(self, player):
+        self.player = player
+
     def first_deal(self, deck):
+        self.player = Player(self.player.name)
         my_hand = []
         deck = Deck()
         my_hand.append(deck.make()[0])
         my_hand.append(deck.make()[1])
+        return my_hand
+
+    def score_count(self, hand):
+        self.hand = hand
+        hand = Hand(self.player.name)
         if my_hand[0][0] in range(2, 10):
-            self.hand_score += my_hand[0][0]
+            self.player.hand_score += my_hand[0][0]
         else:
-            self.hand_score += 10
+            self.player.hand_score += 10
         if my_hand[1][0] in range(2, 10):
-            self.hand_score += my_hand[1][0]
+            self.player.hand_score += my_hand[1][0]
         else:
-            self.hand_score += 10
-        self.score += self.hand_score
+            self.player.hand_score += 10
+        self.player.score += self.player.hand_score
         # print('Рука игрока {} равна {} очков!'.format(self.name, score))
-        if self.score < 22:
-            print('Игрок {} У Вас {} очков'.format(self.name, self.score))
+        if self.player.score < 22:
+            print('Игрок {} У Вас {} очков'.format(self.player.name, self.player.score))
             ans = input('Ещё? ')
             if ans == 'да':
                 self.first_deal(deck)
+                print('Игрок {} У Вас {} очков'.format(self.player.name, self.player.score))
         else:
             print('Перебор, Вы проиграли!')
             # break
-        return my_hand, self.score
+        return my_hand, self.player.score
+
+
+
 
 deck = Deck()
 
@@ -63,7 +76,10 @@ print(my_deck)
 print()
 print('Создаю игроков')
 me = Player('I am')
+my_hand = Hand(me)
 dealer = Player('Дилер')
-print('Моя рука {}'.format(me.first_deal(my_deck)))
-print()
-print('Рука дилера {}'.format(dealer.first_deal(my_deck)))
+print('Моя рука {}'.format(my_hand.first_deal(my_deck)))
+print(my_hand.score_count(my_hand))
+# print('Моя рука {}'.format(me.first_deal(my_deck)))
+# print()
+# print('Рука дилера {}'.format(dealer.first_deal(my_deck)))
