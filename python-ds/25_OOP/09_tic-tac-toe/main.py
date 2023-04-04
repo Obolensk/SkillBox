1e1af64f641d77617adb3e85751472ba9ce15398
Obolensk <menshaev@yandex.ru>
2023-04-04 18:02:17 +0300

    The task 25_08 is not done yet.  I finally confused  Next time I'll have to delete all my code and run to start again

--- python-ds/25_OOP/09_tic-tac-toe/main.py
+++ python-ds/25_OOP/09_tic-tac-toe/main.py
@@ -1,1 +1,19 @@
 # TODO здесь писать код
+class Cell:
+    def __init__(self, number, value):
+        self.number = number
+        self.value = value
+class Board(Cell):
+    # def __init__(self):
+        def new_cell(self):
+            my_board = []
+            new_one = self.value
+            for i in range(9):
+                my_board.append(new_one)
+
+print(Board)
+print(Board.new_cell())
+# print(Cell.new_cell())
+
+# for i in Board.new_cell():
+#     print(i)
