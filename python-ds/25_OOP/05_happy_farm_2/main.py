42f8e6c3364863f3b4ec940b78861f0149486549
Obolensk <menshaev@yandex.ru>
2023-04-12 18:02:07 +0300

    The task 23_05 is not done yet.

--- python-ds/25_OOP/05_happy_farm_2/main.py
+++ python-ds/25_OOP/05_happy_farm_2/main.py
@@ -1,57 +1,38 @@
-# TODO здесь писать код
+class Gardener:
+    ...
+
 class Potato:
-    states = {
-        0: 'Отсутствует',
-        1: 'Росток',
-        2: 'Зеленая',
-        3: 'Зрелая'
-    }
+    states = {0: 'Отсутствует', 1: 'Росток', 2: 'Зеленая', 3: 'Зрелая'}
+
     def __init__(self, index):
+        self.name = 'картошка'
         self.index = index
         self.state = 0
+
     def grow(self):
         if self.state < 3:
             self.state += 1
         self.print_state()
+
     def is_ripe(self):
         if self.state == 3:
             return True
-        else:
-            return False
+        return False
+
     def print_state(self):
-        print('Картошка {} сейчас {}'.format(
-            self.index, Potato.states[self.state]
-        ))
+        print(f'Картошка {self.index} сейчас {Potato.states[self.state]}')
 
-class PotatoGarden:
-    def __init__(self, count):
-        self.potatoes = [Potato(index) for index in range(1, count + 1)]
-    def grow_all(self):
-        print('Картошка прорастает!')
-        for i_potato in self.potatoes:
-            i_potato.grow()
-    def are_all_ripe(self):
-        if not all([i_potato.is_ripe() for i_potato in self.potatoes]):
-            print('Картошка еще не созрела!\n')
-        else:
-            print('Вся картошка созрела! Можно собирать!')
-class Gardener:
-    def __init__(self, name, garden):
-        self.name = name
-        self.garden = garden
 
-    def take_care(self, garden):
-        self.garden = garden
-        garden = PotatoGarden(5)
-        while not garden.are_all_ripe():
-            garden.grow_all()
-            garden.are_all_ripe()
+class Gardenbed:
+    ...
 
-my_garden = PotatoGarden(5)
-ivan = Gardener('Ivan', my_garden)
-ivan.take_care(my_garden)
 
-# my_garden.are_all_ripe()
-# for _ in range(3):
-#     my_garden.grow_all()
-#     my_garden.are_all_ripe()
+my_garden = Gardenbed(5)
+my_garden.are_all_ripe()
+my_gardener = Gardener(name='Боря', garden_bed=my_garden)
+my_gardener.take_care_garden_bed()
+my_gardener.about_me()
+my_gardener.take_care_garden_bed()
+my_gardener.harvest()
+my_gardener.about_me()
+
