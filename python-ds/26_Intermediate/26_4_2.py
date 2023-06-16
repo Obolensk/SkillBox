ba46c2694552ed77197a68731b6ac67b83f2cff3
Obolensk <menshaev@yandex.ru>
2023-06-16 11:43:36 +0300

    The 26_4_2 task is done!

--- python-ds/26_Intermediate/26_4_2.py
+++ python-ds/26_Intermediate/26_4_2.py
@@ -4,4 +4,69 @@ class CanFly:
         self.high = high
         self.speed = speed
 
-    def TakeOff(self):
+    def take_off(self):
+        pass
+
+    def fly(self):
+        pass
+
+    def landing(self):
+        self.high = 0
+        self.speed = 0
+        self.info()
+
+    def info(self):
+        print('Высота равна:', self.high)
+        print('Скорость равна:', self.speed)
+        print('------------------------------')
+
+class ButterFly(CanFly):
+    def __init__(self, high, speed):
+        super().__init__(high, speed)
+
+    def take_off(self):
+        self.high = 1
+        self.speed = 0.5
+        print()
+        print('Бабочка ВЗЛЕТЕЛА!')
+        self.info()
+
+    def fly(self):
+        self.high = 1
+        self.speed = 0.5
+        print()
+        print('Бабочка ЛЕТИТ!')
+        self.info()
+
+class Rocket(CanFly):
+    def __init__(self, high, speed):
+        super().__init__(high, speed)
+
+    def take_off(self):
+        self.high = 500
+        self.speed = 1000
+        print()
+        print('Ракета ВЗЛЕТЕЛА!')
+        self.info()
+
+    def landing(self):
+        self.high = 0
+        self.speed = 0
+        print()
+        print('Ракета ПРИЗЕМЛИЛАСЬ!')
+        print('BOOOM!!!')
+        self.info()
+
+
+
+
+but = ButterFly(0, 0)
+but.take_off()
+but.fly()
+
+r = Rocket(0, 0)
+r.take_off()
+r.landing()
+
+
+
