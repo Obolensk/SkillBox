1affd0d58869a7038dcfda4f98dc650d7a9a5409
Obolensk <menshaev@yandex.ru>
2023-08-16 09:22:58 +0300

    The task 25.2.1 is done. File was added.

---
+++ python-ds/25_26_Repeat/25.2.1.py
@@ -0,0 +1,19 @@
+import random
+
+class Citroen:
+    color = 'black'
+    price = 10 ** 6
+    max_speed = 200
+    current_speed = 0
+
+c3 = Citroen()
+c4 = Citroen()
+c5 = Citroen()
+
+c3.current_speed = random.randint(0, 200)
+c4.current_speed = random.randint(0, 200)
+c5.current_speed = random.randint(0, 200)
+
+print(c3.current_speed)
+print(c4.current_speed)
+print(c5.current_speed)
