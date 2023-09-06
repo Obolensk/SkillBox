1335f2007fc2e942c2457cd4c20d57a751caca6e
Obolensk <menshaev@yandex.ru>
2023-09-06 13:49:17 +0300

    The task 25.6.9 is not done yet (tic tac toe). The task 26.3.2 is note done yet
    I'll going to task 26.4.1

--- python-ds/25_26_Repeat/26.3.3.py
+++ python-ds/25_26_Repeat/26.3.3.py
@@ -3,7 +3,7 @@ list = []
 with open('C:/Users/e.menshaev/Desktop/Skillbox/python-ds/25_26_Repeat/nums.txt', 'r', encoding='utf-8') \
         as file:
     for i in file.read().split():
-        print(i)
+        # print(i)
         list.append(i)
 
 print(list)
@@ -11,23 +11,25 @@ print(list)
 class DivisionError(Exception):
     pass
 
+def my_func(my_list):
+    for i in range(0, len(my_list), 2):
+        print(my_list[i])
+        if int(my_list[i]) / int(my_list[i+1]) > 1:
+            print(int(list[i]), '/', int(list[i+1]), '=', int(list[i]) / int(list[i+1]))
+            return True
+        raise DivisionError('Exception')
+        # print('ERROR!!!!')
 
 # for i in range(0, len(list), 2):
 #     print(i)
-#     if int(list[i]) / int(list[i+1]) > 1:
-#         print(int(list[i]), '/', int(list[i+1]), '=', int(list[i]) / int(list[i+1]))
-#     else:
-#         DivisionError('Exception')
-#         # print('ERROR!!!!')
-
-for i in range(0, len(list), 2):
-    print(i)
-    try:
-        int(list[i]) / int(list[i+1])
-        print(int(list[i]), '/', int(list[i + 1]), '=', int(list[i]) / int(list[i + 1]))
-    except DivisionError:
-        print('нельзя делить большее на меньшее')
+#     try:
+#         int(list[i]) / int(list[i+1])
+#         print(int(list[i]), '/', int(list[i + 1]), '=', int(list[i]) / int(list[i + 1]))
+#     except DivisionError:
+#         print('нельзя делить большее на меньшее')
     #     print(int(list[i]), '/', int(list[i+1]), '=', int(list[i]) / int(list[i+1]))
     # else:
     #     DivisionError('Exception')
     #     # print('ERROR!!!!')
+
+my_func(list)
