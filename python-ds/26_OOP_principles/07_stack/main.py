505ee743807142ee76bd45059e541084ce268a74
Obolensk <menshaev@yandex.ru>
2023-09-20 17:25:39 +0300

    The task 26.6.6 still not done yet. The task 26.6.7 still not done yet.

--- python-ds/26_OOP_principles/07_stack/main.py
+++ python-ds/26_OOP_principles/07_stack/main.py
@@ -1,1 +1,25 @@
 # TODO здесь писать код
+
+my_stack = {}
+
+class Stack:
+
+    # def add_the_task(self):
+    #     my_stack
+
+    def del_the_task(self):
+        pass
+
+
+class TaskManager(Stack):
+    def __init__(self, stack):
+        self.stack = stack
+
+    def new_task(self, task, priority):
+        my_stack[priority] = task
+        # self.stack.add_the_task(task)
+
+
+print(my_stack)
+
+print(my_stack)
