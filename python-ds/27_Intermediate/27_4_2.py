bb4481b6544e9422c0f9d914a391cb180ece3e59
Obolensk <menshaev@yandex.ru>
2023-10-12 19:31:34 +0300

    The task 27_4_2 not done yet. File was added

---
+++ python-ds/27_Intermediate/27_4_2.py
@@ -0,0 +1,18 @@
+
+l = [1, 2, 3, 4, 5]
+
+i = iter(l)
+
+print(l)
+print(i)
+# next(l)
+print(next(i))
+print(next(i))
+print(next(i))
+print(next(i))
+print(next(i))
+
+print()
+print(type(l))
+print(type(i))
+
