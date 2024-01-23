d4e6e6960f191ee72bedfadb07e6c74ccbda8a07
Obolensk <menshaev@yandex.ru>
2024-01-23 18:35:14 +0300

    some change in file 27_4_2_new.py

--- python-ds/27_Intermediate/27_4_1.py
+++ python-ds/27_Intermediate/27_4_1.py
@@ -1,4 +1,5 @@
 
+
 count = 0
 
 def СountIterator():
--- python-ds/27_Intermediate/27_4_2_new.py
+++ python-ds/27_Intermediate/27_4_2_new.py
@@ -1,6 +1,7 @@
 
 import random
 elem = 0
+count_num = 1
 def my_list(nums):
     for i in range(nums):
         global elem
@@ -12,4 +13,6 @@ count = int(input('Введите количество элементов: '))
 print(my_list(count))
 
 for i in my_list(count):
+    print(count_num, end='  ---  ')
     print(i)
+    count_num += 1
