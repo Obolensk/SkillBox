b9abe4d1f7ef3b238783524bf1398b29b596b1e6
EKF <menshaev@yandex.ru>
2024-10-29 21:23:29 +0300

    The tasks 11.6.8 was done finally New file was added!!!

--- python-ds/06-11_2024/11.6.8_new.py
+++ python-ds/06-11_2024/11.6.8_new.py
@@ -1,13 +1,25 @@
 # h = int(input('Введите число: '))
-h = 5
+h = 10
 
-for row in range(1, h+1):
-    for col in range(h*2+1):
-        if row * 2 <= col + row <= h * 2:
-            print('.', end='')
+for row in range(h):
+    for col in range(h*2):
+        if row * 2 + 1 > col + row:
+            print(h - col, end='')
+        elif col + row > h * 2 - 2:
+            print(col - h + 1, end='')
         else:
-            print(abs(h-col), end='')
+            print('.', end='')
     print()
+
+#
+# for row in range(h):
+#     for col in range(h*2):
+#         if row * 2 < col + row < h * 2 - 1 :
+#             print('.', end='')
+#         else:
+#             print(abs(h-col), end='')
+#     print()
+
 
 #
 # for row in range(1, h+1):
