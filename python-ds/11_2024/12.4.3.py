70310b9656bf4f7e433bd09efdce4a15a7ac1edd
EKF <menshaev@yandex.ru>
2024-10-31 18:25:20 +0300

    The tasks 12.4.3 was done New file was added

---
+++ python-ds/11_2024/12.4.3.py
@@ -0,0 +1,13 @@
+import math
+
+x = float(input('Введите число: '))
+
+print(math.floor(x))
+print(math.ceil(x))
+print(abs(x))
+print(math.exp(x))
+print(math.log(x))
+print(math.log2(x))
+print(math.log10(x))
+print(math.sin(x))
+print(math.cos(x))
