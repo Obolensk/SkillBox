03c2c54e8d4d3b0fb418bb50b13e64cb4bea6891
EKF <menshaev@yandex.ru>
2024-11-05 18:37:32 +0300

    The tasks 13.3.1 was done New file was added

---
+++ python-ds/11_2024/13.3.1.py
@@ -0,0 +1,14 @@
+
+def aboutWater (water_price):
+    print('Название: КлирВотер')
+    print('Производитель: ВодЗавод')
+    print('Цена', water_price)
+
+price = int(input('Введите цену воды: '))
+aboutWater(price)
+
+price = int(input('Введите цену воды: '))
+aboutWater(price)
+
+price = int(input('Введите цену воды: '))
+aboutWater(price)
