5ccbf8f3e43b34298dccf38777a4cae5ad2929fd
EKF <menshaev@yandex.ru>
2024-11-05 18:32:19 +0300

    The tasks 13.2.1 was done New file was added

---
+++ python-ds/11_2024/13.2.3.py
@@ -0,0 +1,13 @@
+
+vasya = {'Фамилия': 'Иванов',
+'Имя': 'Василий',
+'Улица': 'Пушкина',
+'Дом': 32
+ }
+def print_your_data(name):
+    print(name)
+    print(name)
+    print(name)
+
+
+print_your_data(vasya)
