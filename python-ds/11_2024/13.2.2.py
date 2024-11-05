502a42109ca5b0357c0ddd0c6a812d70ccd15f8a
EKF <menshaev@yandex.ru>
2024-11-05 18:24:06 +0300

    The tasks 13.2.1 was done New file was added

---
+++ python-ds/11_2024/13.2.2.py
@@ -0,0 +1,13 @@
+
+
+def adding():
+    a = int(input())
+    b = int(input())
+    print("Всего", a + b, "шт.")
+
+print("Сколько мешков рыбы и мяса?")
+adding()
+print("Сколько буханок белого и чёрного хлеба?")
+adding()
+print("Сколько вёдер воды и молока?")
+adding()
