d3d713f1504da6035b5052f798f6c240817c1a9c
EKF <menshaev@yandex.ru>
2024-11-05 18:18:40 +0300

    The tasks 13.2.1 was done New file was added

---
+++ python-ds/11_2024/13.2.1.py
@@ -0,0 +1,9 @@
+
+def greeting():
+    print('Привет!')
+    print('Добро пожаловать!')
+while True:
+    a = input('Зайдёте? Да/Нет: ')
+    if a == 'Да':
+        greeting()
+    print('Следующий.\n')
