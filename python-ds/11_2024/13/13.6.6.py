b60d4389328e9505a714c0e043064c674572b8e0
EKF <menshaev@yandex.ru>
2024-11-06 18:16:27 +0300

    The tasks 13.6.6 was done New file was added

---
+++ python-ds/11_2024/13/13.6.6.py
@@ -0,0 +1,9 @@
+
+
+def find_point(x, y):
+    if -1 <= x <= 1 and -1 <= y <= 1:
+        print('Монетка где-то рядом')
+    else:
+        print('Монетки в области нет')
+
+find_point(-0.1, 0.4351)
