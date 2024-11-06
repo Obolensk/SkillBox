30bbd9b94397708836ab0e9eb07472f76d1a6009
EKF <menshaev@yandex.ru>
2024-11-06 11:55:52 +0300

    The tasks 13.4.2 was done New file was added

---
+++ python-ds/11_2024/13.4.2.py
@@ -0,0 +1,14 @@
+
+def adress(surname, name, country, city, street, house, apartment):
+    print()
+    print('Фамилия:', surname)
+    print('Имя:', name)
+    print('Страна:', country)
+    print('Город:', city)
+    print('Улица:', street)
+    print('Дом:', house)
+    print('Квартира:', apartment)
+
+adress('Иванов', 'Сергей', 'РФ', 'Москва', 'Рождественская', '1', '410')
+adress('Степанов', 'Иван', 'РФ', 'Химки', 'Новозаводская', '12', '23')
+adress('Петров', 'Степан', 'РФ', 'Оболенск', 'Биологов', '7', '11')
