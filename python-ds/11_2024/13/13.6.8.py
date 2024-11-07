8b31bfa047e0ddb24c693dbd3d34e59651a12adb
EKF <menshaev@yandex.ru>
2024-11-07 10:30:43 +0300

    The tasks 13.6.8 was't done New file was added

--- python-ds/11_2024/13/13.6.8.py
+++ python-ds/11_2024/13/13.6.8.py
@@ -1,9 +1,12 @@
 
 
-a = int(input('Введите первое число:'))
-b = int(input('Введите втророе число:'))
+a = int(input('Введите первое число: '))
+b = int(input('Введите втророе число: '))
 
-if a > b:
-    for i in range(b, 0, -1):
-        if a % i == 0 and b % i == 0:
-            print('')
+# print(max(a, b))
+# print(min(a, b))
+#
+for i in range(max(a, b), 0, -1):
+    if max(a, b) % i == 0 and min(a, b) % i == 0:
+        print('НОД равен', i)
+        break
