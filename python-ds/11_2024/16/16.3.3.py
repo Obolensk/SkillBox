32c33bbb2a68d727ed3364ef05c754174703b4d8
EKF <menshaev@yandex.ru>
2024-11-12 11:45:25 +0300

    The tasks 16.3.3 was done New file was added

---
+++ python-ds/11_2024/16/16.3.3.py
@@ -0,0 +1,26 @@
+task = []
+text = []
+word = str()
+
+def word_count(word, text):
+    count = 0
+    for i in text:
+        if word == i:
+            count += 1
+    return count
+
+for i in range(1, 4):
+    print('Введите', i, 'слово: ', end='')
+    word = input()
+    task.append(word)
+
+print()
+while word != 'end':
+    word = input('Слово из текста: ')
+    text.append(word)
+
+print('\nПодсчет слов в тексте')
+
+print(task[0], ':', word_count((task[0]), text))
+print(task[1], ':', word_count((task[1]), text))
+print(task[2], ':', word_count((task[2]), text))
