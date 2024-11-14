e098323d6efaab564cf5dfaec67c7a2f3d8d93df
EKF <menshaev@yandex.ru>
2024-11-14 20:20:54 +0300

    The task 17.3.2 was done New file was added

--- python-ds/11_2024/17/17.3.2.py
+++ python-ds/11_2024/17/17.3.2.py
@@ -1,1 +1,22 @@
 
+
+def sym_count(text, first_sym, second_sym):
+    count = 0
+    for sym in text:
+        if sym == first_sym or sym == second_sym:
+            count += 1
+    return count
+
+# print(sym_count('Привет, Андрей', 'е', 'й'))
+
+first_mes = input('Первое сообщение: ')
+second_mes = input('Второе сообщение: ')
+
+if sym_count(first_mes, '!', '?') > sym_count(second_mes, '!', '?'):
+    print(first_mes, second_mes)
+elif sym_count(first_mes, '!', '?') < sym_count(second_mes, '!', '?'):
+    print(second_mes, first_mes)
+else:
+    print('Ой!')
+
+
