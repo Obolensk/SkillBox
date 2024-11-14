83f7a68e20995813922b1c953e9ac358e760a24d
EKF <menshaev@yandex.ru>
2024-11-14 19:45:20 +0300

    The task 17.2.2 was done New file was added

--- python-ds/11_2024/17/17.2.2.py
+++ python-ds/11_2024/17/17.2.2.py
@@ -1,1 +1,17 @@
 
+num = int(input('Кол-во сотрудников: '))
+sal_list = []
+
+for i in range(num):
+    print('Зарплата', i, 'сотрудника: ', end='')
+    salary = int(input())
+    sal_list.append(salary)
+
+print(sal_list)
+
+for i in sal_list:
+    if i == 0:
+        sal_list.remove(i)
+
+print('\nОсталось сотрудников:', len(sal_list))
+print(sal_list)
