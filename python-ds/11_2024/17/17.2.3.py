795dee6986bade84a5644d14a10d55ca09a05d8c
EKF <menshaev@yandex.ru>
2024-11-14 19:53:22 +0300

    The task 17.2.3 was done New file was added

--- python-ds/11_2024/17/17.2.3.py
+++ python-ds/11_2024/17/17.2.3.py
@@ -1,1 +1,19 @@
 
+film_list = []
+
+while True:
+    print('\nВаш текущий топ фильмов:', film_list)
+    film = input('Название фильма: ')
+    print('Команды: добавить, вставить, удалить')
+    command = input('Введите команду: ')
+    if command == 'добавить':
+        film_list.append(film)
+    elif command == 'вставить':
+        film_list.insert(0, film)
+    elif command == 'удалить':
+        film_list.remove(film)
+
+
+
+
+
