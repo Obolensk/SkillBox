cf06f21f33ea8b16522e0b8b0919102cb1b63559
e.menshaev <menshaev@yandex.ru>
2025-01-24 20:01:31 +0300

    The tasks 19.6.10 was almost done for one of examples. I have to do this task New file was added

---
+++ python-ds/11_2024/19/19.6.10.py
@@ -0,0 +1,37 @@
+
+#
+text = ('vujgvmCfb tj ufscfu ouib z/vhm jdjuFyqm '
+        'jt fscfuu uibo jdju/jnqm fTjnqm tj scfuuf ibou fy/dpnqm yDpnqmf jt '
+        'cfuufs boui dbufe/dpnqmj uGmb tj fuufsc ouib oftufe/ bstfTq jt uufscf uibo otf/ef uzSfbebcjmj vout/dp djbmTqf '
+        'dbtft (ubsfo djbmtqf hifopv up csfbl ifu t/svmf ipvhiBmu zqsbdujdbmju fbutc uz/qvsj Fsspst tipvme wfsof qbtt '
+        'foumz/tjm omfttV mjdjumzfyq odfe/tjmf Jo fui dfgb pg hvjuz-bncj gvtfsf fui ubujpoufnq up ftt/hv Uifsf vmetip '
+        'fc pof.. boe sbcmzqsfgf zpom pof pvt..pcwj xbz pu pe ju/ Bmuipvhi uibu bzx bzn puo cf wjpvtpc bu jstug ttvomf '
+        'sfzpv( i/Evud xOp tj scfuuf ibou /ofwfs uipvhiBm fsofw jt fopgu cfuufs boui iu++sjh x/op gJ ifu '
+        'nfoubujpojnqmf tj eibs pu mbjo-fyq tju( b bec /jefb Jg fui foubujpojnqmfn jt fbtz up bjo-fyqm ju znb cf b '
+        'hppe jefb/ bnftqbdftO bsf pof ipoljoh sfbuh efbj .. fu(tm pe psfn gp tf"uip')
+
+# text = 'asdfgh kjh sdf'
+
+abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+       'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+# print(abc.index('c'))
+shift = 25
+
+new_text = []
+str = ''
+
+for i in text:
+    if i == ' ':
+        new_text.append(' ')
+    elif i in abc:
+        # print(text.index(i))
+        new_text.append(abc[(abc.index(i) + shift) % len(abc)])
+    else:
+        new_text.append(i)
+    # print(new_text)
+    for i in new_text:
+        print(i, end='')
+    new_text = []
+
+# print(new_text)
