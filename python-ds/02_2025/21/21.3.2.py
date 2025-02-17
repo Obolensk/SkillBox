10247fa78122cf45bd8e58a9e8c9da91b60567bf
EKF <menshaev@yandex.ru>
2025-02-17 19:14:09 +0300

    The tasks 21.3.2 was done New file was added

--- python-ds/02_2025/21/21.3.2.py
+++ python-ds/02_2025/21/21.3.2.py
@@ -1,3 +1,14 @@
-# Первый список: ['й', 'р', 'с', 'г', 'а', 'а', 'т', 'ж', 'е', 'к']
-#
-# Второй список: ['д', 'а', 'а', 'в', 'т', 'ж', 'р', 'б', 'й', 'р']
+first = ['й', 'р', 'с', 'г', 'а', 'а', 'т', 'ж', 'е', 'к']
+second = ['д', 'а', 'а', 'в', 'т', 'ж', 'р', 'б', 'й', 'р']
+
+# first_dict = dict()
+# second_dict = dict()
+
+def make_dict(my_list):
+    my_dict = dict()
+    for item in enumerate(my_list):
+        my_dict[item[0]] = item[1]
+    return my_dict
+
+print('Первый словарь:', make_dict(first))
+print('Второй словарь:', make_dict(second))
