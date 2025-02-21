346641019edb40ea8a449e572192c8b31d819af1
e.menshaev <menshaev@yandex.ru>
2025-02-21 12:10:18 +0300

    The task 21.8.7 was done. New file was added

--- python-ds/02_2025/21/21.8.7.py
+++ python-ds/02_2025/21/21.8.7.py
@@ -3,23 +3,29 @@
 tup_1 = (6, 8, 5, 1, 3, 2, 0, 9, 4, 7)
 tup_2 = (6, 8, 5, 1, 3, '2', 0, 9, 4, 7)
 new_tup = []
+
 # print(tuple(sorted(list(tup))))
 # print(sorted(list(tup_1)))
 
-def tup_func(tup):
+def tup_func(tup, a = 0):
     lst = []
     for num in tup:
         # print(num, type(num))
         if type(num) != int:
             print('В кортеже не все элементы являются целыми числами')
-            return tup
+            a = 1
             break
         else:
             lst.append(num)
-    return sorted(lst)
+            a = 0
+    if a == 0:
+        return sorted(lst)
+    else:
+        return tup
 
-
+print('\n\ttup 1')
 print(tup_func(tup_1))
+print('\n\ttup 2')
 print(tup_func(tup_2))
 
 
