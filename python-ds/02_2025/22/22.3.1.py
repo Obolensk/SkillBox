62afd0662d7ffa0b48dc3d2917e58512f7ad498b
e.menshaev <menshaev@yandex.ru>
2025-02-24 18:33:29 +0300

    The task 22.3.1 was not done yet. New file was added

---
+++ python-ds/02_2025/22/22.3.1.py
@@ -0,0 +1,21 @@
+import random
+
+
+def change_dict(dct):
+    num = random.randint(1, 100)
+    for i_key, i_value in dct.items():
+        if isinstance(i_value, list):
+            i_value.append(num)
+        if isinstance(i_value, dict):
+            i_value[num] = i_key
+        if isinstance(i_value, set):
+            i_value.add(num)
+
+
+nums_list = [1, 2, 3]
+some_dict = {1: 'text', 2: 'another text'}
+uniq_nums = {1, 2, 3}
+common_dict = {1: nums_list, 2: some_dict, 3: uniq_nums, 4: (10, 20, 30)}
+
+change_dict(common_dict)
+print(common_dict)
