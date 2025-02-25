4bf27f4614b6f8bc10504f3eee1e959df111df30
e.menshaev <menshaev@yandex.ru>
2025-02-25 10:09:14 +0300

    The task 22.3.1 was done. New file was added

--- python-ds/02_2025/22/22.3.1.py
+++ python-ds/02_2025/22/22.3.1.py
@@ -1,8 +1,10 @@
 import random
+import copy
 
 
 def change_dict(dct):
     num = random.randint(1, 100)
+    new_dct = copy.deepcopy(dct)
     for i_key, i_value in dct.items():
         if isinstance(i_value, list):
             i_value.append(num)
@@ -10,12 +12,22 @@ def change_dict(dct):
             i_value[num] = i_key
         if isinstance(i_value, set):
             i_value.add(num)
+    return new_dct
 
 
 nums_list = [1, 2, 3]
 some_dict = {1: 'text', 2: 'another text'}
 uniq_nums = {1, 2, 3}
-common_dict = {1: nums_list, 2: some_dict, 3: uniq_nums, 4: (10, 20, 30)}
+common_dict = {1: nums_list[:], 2: some_dict.copy(), 3: uniq_nums.copy(), 4: (10, 20, 30)}
 
 change_dict(common_dict)
 print(common_dict)
+print('----------------------------------------')
+print()
+for k, v in common_dict.items():
+    print('{}: {}'.format(k, v))
+
+print()
+print(list(nums_list))
+print(some_dict)
+print(uniq_nums)
