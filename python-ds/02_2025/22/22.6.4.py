3ea7bb320e253b338e7e21224243f80b50cf5995
e.menshaev <menshaev@yandex.ru>
2025-03-10 18:12:22 +0300

    The task 22.6.4 was not done yet. New file was added

---
+++ python-ds/02_2025/22/22.6.4.py
@@ -0,0 +1,30 @@
+site = {
+    'html': {
+        'head': {
+            'title': 'Мой сайт'
+        },
+        'body': {
+            'h2': 'Здесь будет мой заголовок',
+            'div': 'Тут, наверное, какой-то блок',
+            'p': 'А вот здесь новый абзац'
+        }
+    }
+}
+
+print(site)
+print('html' in site)
+print()
+
+find_key = input('Введите искомый ключ: ')
+
+
+def finder(struct, key):
+    if key in struct:
+        return struct[key]
+    for keys in struct.values():
+        print(struct.values())
+        finder(keys, key)
+
+
+
+print(finder(site, find_key))
