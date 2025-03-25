890bb462fa939871075205e68b91df5903a857cb
Obolensk <menshaev@yandex.ru>
2025-03-25 23:11:36 +0300

    New file was added 23.1.1 The task was done

--- python-ds/03_2025/23/23.1.1.py
+++ python-ds/03_2025/23/23.1.1.py
@@ -6,3 +6,10 @@ file_name = 'admin.bat'
 print(os.path.abspath(file_name))
 print(os.path.relpath(file_name))
 
+abs_path = os.path.abspath(file_name)
+print('   Абсолютный путь до файла:', abs_path)
+
+rel_path = os.path.join(file_name)
+print('Относительный путь до файла:', rel_path)
+
+
