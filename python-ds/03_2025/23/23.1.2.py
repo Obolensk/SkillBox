4ace496e85a8c8f02855f1b803d03ec54b28ae13
e.menshaev <menshaev@yandex.ru>
2025-04-01 18:32:26 +0300

    The task 23.3.1 was done. New files was added

--- python-ds/03_2025/23/23.1.2.py
+++ python-ds/03_2025/23/23.1.2.py
@@ -1,5 +1,17 @@
 import os
 
+my_dir = 'C:/Users/e.menshaev/PycharmProjects/SkillBox/python-ds'
+new_dir = os.path.dirname(my_dir)
+new_dir_1 = os.path.dirname(new_dir)
+
+print(my_dir)
+print(new_dir)
+print(new_dir_1)
+print()
+
+print(os.listdir(new_dir_1))
+import os
+
 folder = 'C:/Users/ASUS/Desktop/SkillBox'
 my_dir = os.listdir(folder)
 print('\nСодержимое каталога', os.path.abspath(folder))
---
+++ python-ds/03_2025/23/23_3_1/task/23.3.1.py
@@ -0,0 +1,35 @@
+file = open('group_1.txt', 'r', encoding='utf-8')
+summa = 0
+
+for i_line in file:
+    info = i_line.split()
+    print(info)
+    summa += int(info[2])
+file.close()
+
+print(summa)
+#
+file = open('group_1.txt', 'r', encoding='utf-8')
+diff = 0
+#
+for i_line in file:
+    info = i_line.split()
+    print(info)
+    diff -= int(info[2])
+file.close()
+
+print(diff)
+#
+file_2 = open('C:/Users/e.menshaev/PycharmProjects/SkillBox/python-ds/03_2025/23/23_3_1/task/Additional_info/group_2.txt', 'r', encoding='utf-8')
+compose = 1
+
+for i_line in file_2:
+    info = i_line.split()
+    print(info)
+    compose *= int(info[2])
+
+print(summa)
+print(diff)
+print(compose)
+
+
